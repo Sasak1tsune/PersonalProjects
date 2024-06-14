bb89bc365506fd955f87f9d9e6306675ecdcaf62
Juan Pablo Perea Santos <jppereas@cosentino.com>
2024-06-14 14:56:21 +0200

    layout done

--- ssk-toolbox/src/app/app.component.ts
+++ ssk-toolbox/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './homePage/home/home.component';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { featherSearch} from '@ng-icons/feather-icons';
+import { ionLogoGoogle} from '@ng-icons/ionicons';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HomeComponent, NgIconComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  viewProviders: [provideIcons({ featherSearch, ionLogoGoogle})]
 })
 export class AppComponent {
   title = 'ssk-toolbox';
--- ssk-toolbox/src/app/homePage/home/search-bar/search-bar.component.ts
+++ ssk-toolbox/src/app/homePage/home/search-bar/search-bar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NgIconComponent } from '@ng-icons/core';
 
 @Component({
   selector: 'ssk-searchbar',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
-
+  
 }
